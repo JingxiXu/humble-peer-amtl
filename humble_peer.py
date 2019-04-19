26b8ac8fbfe4e1777fe43a1e6e0d84b9f04542ce
Jingxi Xu <zealliant@outlook.com>
2019-04-19 13:08:37 -0400

    add sharing data; reduce number of queries

--- humble_peer.py
+++ humble_peer.py
@@ -272,8 +272,8 @@ def new_peer(X, Y, X_test, Y_test, k, fea, mode, query_limit = 10**10, sparse=Fa
     peer_count = 0.0
     b = 1  # controls self confidence
     alpha = 1.1
-    # C = np.log(30)
-    C = 0
+    C = np.log(30)
+    # C = 0
     beta = 0.1
     acc = []
 
@@ -362,27 +362,23 @@ def new_peer(X, Y, X_test, Y_test, k, fea, mode, query_limit = 10**10, sparse=Fa
     # print(np.average(err_count_tasks), np.std(err_count_tasks))
     return acc
 
-def new_new_peer(X, Y, X_test, Y_test, k, fea, query_limit=10**10, sparse=False, run=30):
+def my_peer(X, Y, X_test, Y_test, k, fea, query_limit=10**10, sparse=False, run=30):
 
-    query_count_tasks = []
-    total_count = np.zeros((k, 1))
-    err_count_tasks = []
-    peer_count = 0.0
+    # hyperparameters
     b = 1  # controls self confidence
-    alpha = 1.1
     C = np.log(30)
-    beta = 0.1
-    acc = []
+    # C = 1
+
+    query_count_runs = np.zeros((k, 0))
+    err_count_runs = np.zeros((k, 0))
+    acc_runs = []
 
     for r in range(run):
         shuffle = np.random.permutation(X.shape[0])
-        query_count = 0.0
-        err_count = np.zeros((k, 1))
+        query_count = np.zeros((k, 1)) # query count for each task
+        err_count = np.zeros((k, 1)) # err count for each task
         tau = np.ones((k, k))
         w = np.zeros((k, fea))
-        total_sum = np.zeros((k, fea))
-        total_num = np.zeros((k, 1))
-        center = np.zeros((k, fea))
 
         for i in range(X.shape[0]):
             if sparse:
@@ -397,16 +393,14 @@ def new_new_peer(X, Y, X_test, Y_test, k, fea, query_limit=10**10, sparse=False,
 
             f_t = w.dot(x)
 
-            ### mode being mistake
             f_total = f_t.dot(tau[tid])
-            ####
 
             y_hat = np.sign(f_total)
             if y_hat == 0:
                 y_hat = -1
             uncertainty = b / (b + abs(f_total))
             z = np.random.binomial(1, uncertainty)
-            if query_count >= query_limit:
+            if np.sum(query_count) >= query_limit:
                 z = 0
 
             yt = Y[shuffle[i]]
@@ -416,58 +410,80 @@ def new_new_peer(X, Y, X_test, Y_test, k, fea, query_limit=10**10, sparse=False,
                 err_count[tid] += 1
                 w[tid] = w[tid] + yt * z * x
             if z == 1:
-                query_count += 1
+                query_count[tid] += 1
 
-            ### old being mistake mode
             f_t_sign = np.sign(f_t)
             for i in range(k):
                 if f_t_sign[i] == 0:
                     f_t_sign[i] = -1
-            #####
 
             if z == 1:
-                l_t = np.fmin([2] * k, np.fmax([0] * k, 1 - f_t * yt))
-                l_t = l_t / sum(l_t)
+                l_t = np.fmin([2] * k, np.fmax([0] * k, 1 - f_t * yt)) # TODO what is this?
+                if sum(l_t) != 0:
+                    l_t = l_t / sum(l_t)
                 for tsk in range(k):
                     tau[tid][tsk] = tau[tid][tsk] * np.exp(- C * l_t[tsk])
                 tau[tid] = tau[tid] * k / sum(tau[tid])
 
-            total_count[tid] += 1
-            if query_count >= query_limit:
+            #### my modification
+            # now only share data if true label queried
+            for j in range(k):
+                if tau[tid][j] >= tau[tid][tid] and j!=tid:
+                    if yt != f_t_sign[j]:
+                        w[j] = w[j] + z * yt * x
+
+            if np.sum(query_count) >= query_limit:
                 break
+
+        # one run is finished
         w = tau.dot(w)
-        acc.append(test(w, k, X_test, Y_test, "peer", sparse))
-        query_count_tasks.append(query_count)
-        err_count_tasks.append(np.sum(err_count) / X.shape[0])
-    avg_acc = sum(acc) / len(acc)
+        acc = test(w, k, X_test, Y_test, "peer", sparse)
+        query = np.sum(query_count)
+        mistake_rate = np.sum(err_count)/X_train.shape[0]
+        print("iteration: {} acc: {:.4f} query: {:.4f} mistake_rate: {:.4f}".format(r, acc, query, mistake_rate))
+        acc_runs.append(acc)
+        query_count_runs = np.hstack((query_count_runs, query_count))
+        err_count_runs = np.hstack((err_count_runs, err_count))
+
+    # all runs are finished
+    avg_acc = sum(acc_runs) / len(acc_runs)
+    avg_query_count = np.average(np.sum(query_count_runs, axis=0))
+    avg_mistake_rate = np.average(np.sum(err_count_runs, axis=0))/X_train.shape[0]
     print("{:16s} query {:09.4f}\t mistakes {:.4f}\t accuracy {:.4f}".format("peer",
-                                                                             np.average(query_count_tasks),
-                                                                             np.sum(err_count_tasks) / run,
+                                                                             avg_query_count,
+                                                                             avg_mistake_rate,
                                                                              avg_acc))
     # print(np.average(acc), np.std(acc))
-    # print(np.average(query_count_tasks), np.std(query_count_tasks))
-    # print(np.average(err_count_tasks), np.std(err_count_tasks))
-    return acc
+    # print(np.average(query_count_runs), np.std(query_count_runs))
+    # print(np.average(err_count_runs), np.std(err_count_runs))
+    return acc_runs
 
+def print_dataset_info(args):
+    print("data set: {}".format(args.data_filepath))
+    print("X_train.shape: {}".format(args.X_train.shape))
+    print("Y_train.shape: {}".format(args.Y_train.shape))
+    print("X_test.shape: {}".format(args.X_test.shape))
+    print("Y_test.shape: {}".format(args.Y_test.shape))
+    print("num of tasks: {}".format(args.k))
+    print("num of training data per task: {}".format(args.X_train.shape[0]/k))
+    print("num of test data per task: {}".format(args.X_test.shape[0] / k))
+    print("num of features: {}".format(args.fea-1))
 
+    num_positive_point = np.sum(args.Y_train)+np.sum(args.Y_test)
+    num_data = args.X_train.shape[0] + args.X_test.shape[0]
+    print("num of positive points: {} ({})".format(num_positive_point, num_positive_point/num_data))
 
 if __name__ == "__main__":
     args = get_args()
     X_train, Y_train, X_test, Y_test, k, fea = args.X_train, args.Y_train, args.X_test, args.Y_test, args.k, args.fea
     sparse, run = args.sparse, args.run
 
-    print("data set: {}".format(args.data_filepath))
-    print("X_train.shape: {}".format(X_train.shape))
-    print("Y_train.shape: {}".format(Y_train.shape))
-    print("X_test.shape: {}".format(X_test.shape))
-    print("Y_test.shape: {}".format(Y_test.shape))
-    print("num of tasks: {}".format(k))
-    print("num of features: {}".format(fea))
+    print_dataset_info(args)
 
     # random(X_train, Y_train, X_test, Y_test, k, fea, sparse, run)
     # indep(X_train, Y_train, X_test, Y_test, k, fea, sparse, run)
     # pooled(X_train, Y_train, X_test, Y_test, k, fea, sparse, run)
     # peer(X_train, Y_train, X_test, Y_test, k, fea, sparse=sparse, run=run)
-    new_peer(X_train, Y_train, X_test, Y_test, k, fea, mode='mistake', sparse=sparse)
-    # new_new_peer(X_train, Y_train, X_test, Y_test, k, fea, sparse=sparse, run=run)
+    # new_peer(X_train, Y_train, X_test, Y_test, k, fea, mode='mistake', sparse=sparse)
+    my_peer(X_train, Y_train, X_test, Y_test, k, fea, sparse=sparse, run=run)
 
